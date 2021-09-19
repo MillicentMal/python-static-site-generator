dd1cb2433244fc6fb99c22945d33526b829a8452
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-09-19 03:03:38 +0200

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -7,4 +7,10 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest/path.relative_to(self.source)
-        
+        mkdir(directory, parents=True, exist_ok=True)
+
+
+    def build(self):
+        mkdir(self.dest, parents=True, exist_ok=True)
+
+
